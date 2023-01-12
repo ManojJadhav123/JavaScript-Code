c01b180f9b30446f7ea095f49d10d94c25bebc26
Manoj Jadhav <63336783+ManojJadhav123@users.noreply.github.com>
2023-01-12 13:11:43 +0530

    Add files via upload

---
+++ Arrays/Basic/PrintArray.java
@@ -0,0 +1,13 @@
+// write a programme to print the array elements
+public class PrintArray {
+    public static void main(String[] args) {
+        int arr[] = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        for( int i=0; i<3; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
