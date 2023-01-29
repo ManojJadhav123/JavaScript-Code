1bbe42517aaccf03d1ca6c7f7206317dc9738f9b
Manoj Jadhav <63336783+ManojJadhav123@users.noreply.github.com>
2023-01-29 21:00:16 +0530

    Add files via upload

---
+++ 21_while_loop_example.js
@@ -0,0 +1,20 @@
+// calculate the sum of numbers upto n;
+
+let num = 100;
+
+let total = 0;
+
+let i = 0;
+
+while(i<=100){
+    total = total + i;
+    i++;
+}
+console.log(total);
+
+// second way with the mathematical formula 
+
+// sum of n natural numbers = n*(n+1) / 2;
+
+// total = (num * (num + 1 ))/2;
+// console.log(total);
